c002e86413ec976192e0d3ed35e798ead3157b05
laityts <laityts@gmail.com>
2020-12-29 23:29:56 +0800

    add openclash

--- devices/phicomm-k2p/diy.sh
+++ devices/phicomm-k2p/diy.sh
@@ -6,13 +6,3 @@ rm -Rf target/linux/ramips/.svn
 echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips/patches-5.4 target/linux/ramips/patches-5.4
 
 sed -i 's/O2/Os/g' include/target.mk
-
-sed -i '/app_update/d' package/feeds/custom/luci-app-bypass/luasrc/controller/bypass.lua
-
-sed -i 's/PKG_VERSION:=1/PKG_VERSION:=2/' package/feeds/custom/luci-app-bypass/Makefile
-
-sed -i 's/ +unzip +lua-maxminddb//' package/feeds/custom/luci-app-bypass/Makefile
-
-sed -i '/status_bottom/d' package/feeds/custom/luci-app-bypass/luasrc/model/cbi/bypass/base.lua
-
-rm -Rf package/feeds/custom/luci-app-bypass/{root/www,root/usr/share/bypass/GeoLite2-Country.mmdb}
